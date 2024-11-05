7963a8e953384df43bbd4452d81fd16f50b01bda
justrandomcoding <justrandomcoding@gmail.com>
2024-11-05 22:26:01 +0000

    removed tailwind

---
+++ Blog/Components/Pages/MarkdownConverter.cs
@@ -0,0 +1,30 @@
+using Markdig;
+using Markdig.Extensions.AutoIdentifiers;
+using Microsoft.AspNetCore.Components;
+
+namespace Blog.Components.Pages;
+
+public static class MarkdownConverter
+{
+    private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder()
+        .UseAdvancedExtensions()
+        .UseAutoIdentifiers(AutoIdentifierOptions.GitHub)
+        .UseEmojiAndSmiley()
+        .UseCitations()
+        .UseBootstrap()
+        .Build();
+
+    public static MarkupString ToMarkupString(string markdown)
+    {
+        return string.IsNullOrEmpty(markdown)
+            ? default
+            : (MarkupString)Markdown.ToHtml(markdown, MarkdownPipeline);
+    }
+
+    public static string ToPlainString(string markdown)
+    {
+        return string.IsNullOrEmpty(markdown)
+            ? default
+            : Markdown.ToPlainText(markdown, MarkdownPipeline).TrimEnd('\r', '\n');
+    }
+}
